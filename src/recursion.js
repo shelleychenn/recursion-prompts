2bee09bc7d133596c09e7fb3e2c2640a7f5ae0dd
shelleychenn <shelleychen322@gmail.com>
2020-08-13 12:43:46 -0400

    Complete divide prompt

--- src/recursion.js
+++ src/recursion.js
@@ -154,7 +154,23 @@ var multiply = function (x, y) {
 
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods to arrive at an approximate quotient (ignore decimal endings).
-var divide = function (x, y) {};
+var divide = function (x, y) {
+  if (y === 0) {
+    return NaN;
+  }
+
+  if (x === 0) {
+    return 0;
+  }
+
+  if (x < 0 && y < 0 && x > y) {
+    return 0;
+  } else if (x < y) {
+    return 0;
+  }
+
+  return 1 + divide(x - y, y);
+};
 
 // 14. Find the greatest common divisor (gcd) of two positive numbers. The GCD of two
 // integers is the greatest integer that divides both x and y with no remainder.
